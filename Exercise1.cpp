760d552d232c0be38659e9a62fff7821cbabe041
Mateusz <bartnickimateuszgame@gmail.com>
2021-11-26 08:54:13 +0100

    V1
    just practicin, ingraining how computer memory works

---
+++ Exercise1.cpp
@@ -0,0 +1,12 @@
+#include "Exercise1.h"
+
+void Exercise1::ex1()
+{
+	std::cout << "Input integer a\n";
+	std::cin >> a;
+	std::cout << "Inpit integer b\n";
+	std::cin >> b;
+
+	std::cout << "A = " << a << " ptrA = " << *ptrA << "\n";
+	std::cout << "B = " << b << " ptrB = " << *ptrB << "\n";
+}
---
+++ Exercise1.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <iostream>
+
+class Exercise1
+{
+
+	private:
+
+		int a = 0;
+		int b = 0;
+		int* ptrA = &a;
+		int* ptrB = &b;
+
+	public: 
+		void ex1();
+};
+
